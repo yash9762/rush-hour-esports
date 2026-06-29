727199be16efb0aa8b63e489ce0cb04352a3109e
yash9762 <yashking9762@gmail.com>
2026-06-29 14:31:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function (e) {
-    const target = document.querySelector(this.getAttribute('href'));
-    if (target) {
-      e.preventDefault();
-      target.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
